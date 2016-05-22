8151cb58bab20de081c3436434a475bb03a8773c
Bernardo Martínez Garrido <programming@wandrell.com>
2016-05-22 13:30:17 +0200

    Cursor now changes over icons and abilities names.

---
+++ _static/js/tooltip.js
@@ -0,0 +1,15 @@
+/**
+ * Licensed under the MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+/**
+ * Functions for setting up tooltips.
+ */
+
+/**
+ * Initializes the tooltips.
+ */
+$(document).ready(function () {
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+});
